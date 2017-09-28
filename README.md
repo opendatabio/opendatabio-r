66541d1fd97c3f084509d46f58aa8c4c367ccdb2
Andre Chalom <andrechalom@gmail.com>
2017-09-28 19:37:28 -0300

    First working version of get_taxons

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # opendatabio-r
+
+This package implements a client to the OpenDataBio API. This should allow for users
+with some experience in R to query the database, import and export data, and run common analyses.
+The package is written on top of [httr](http://httr.r-lib.org/) package.
+Advanced users are welcome to write their own requests using the `httr::GET` and `httr::POST` 
+functions directly.
+
+## Install
+
+Installation is currently done only from GitHub. 
+
+```R
+library(devtools)
+install_github("opendatabio/opendatabio-r")
+```
+
+## Basic usage
+
+The first to be done is create a `odb_config` object to hold the configuration for connecting with your
+OpenDataBio server. For anonymous use to the development server, simply use its default configuration:
+
+```R
+cfg = odb_config()
+```
+
+Then, write a request using the [OpenDataBio API documentation](https://github.com/opendatabio/opendatabio/wiki/API),
+and pass the configuration object:
+
+```R
+taxons = odb_get_taxons(list(valid = TRUE, format = "simple"), cfg)
+head(taxons)
+```
---
+++ tests/testthat/test_odb_config.R
@@ -0,0 +1,27 @@
+test_that("odb_config with defaults", { 
+    cfg = odb_config();
+    expect_is(cfg, "list")
+    expect_is(cfg$base_url, "character")
+    expect_equal(cfg$base_url, "http://opendatabio.ib.usp.br/opendatabio/api/v0")
+    expect_is(cfg$headers, "request")
+    expect_match(cfg$headers$headers["Accept"], "application/json")
+    expect_match(cfg$headers$options$useragent, "opendatabio")
+})
+
+test_that("odb_config with parameters", { 
+    cfg = odb_config("http://localhost", "ABCDEF", "v1", c(foo="bar"));
+    expect_is(cfg, "list")
+    expect_is(cfg$base_url, "character")
+    expect_equal(cfg$base_url, "http://localhost/v1")
+    expect_is(cfg$headers, "request")
+    expect_match(cfg$headers$headers["Accept"], "application/json")
+    expect_match(cfg$headers$headers["foo"], "bar")
+    expect_match(cfg$headers$headers["Authorization"], "ABCDEF")
+})
+
+test_that("odb_config with null version", { 
+    cfg = odb_config(api_version = NULL);
+    expect_is(cfg, "list")
+    expect_is(cfg$base_url, "character")
+    expect_equal(cfg$base_url, "http://opendatabio.ib.usp.br/opendatabio/api/") # "stray" / but it's ok
+})
