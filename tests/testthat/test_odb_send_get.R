894a798ac3306e191bfccade84558e3c40d09650
Andre Chalom <andrechalom@gmail.com>
2017-09-29 23:34:45 -0300

    Fixes #2, fixes #3, fixes double slashes

---
+++ tests/testthat/test_odb_send_get.R
@@ -0,0 +1,28 @@
+test_that("odb_send_get with defaults", { 
+    cfg = odb_config("http://httpbin.org", , NULL, c(Accept="text/html"));
+    ret = odb_send_get(list(), cfg, "")
+    expect_is(ret, "response")
+    expect_equal(http_type(ret), "text/html")
+    expect_equal(status_code(ret), 200)
+})
+
+test_that("odb_send_get checks http_type", { 
+    cfg = odb_config("http://httpbin.org", , NULL, c(Accept="application/json; text/html; */*"));
+    ret = odb_send_get(list(), cfg, "headers")
+    expect_is(ret, "response")
+    expect_equal(http_type(ret), "application/json")
+    cfg = odb_config("http://httpbin.org", , NULL, c(Accept="blablabla"));
+    expect_error(odb_send_get(list(), cfg, "headers"), "Wrong answer type from server")
+})
+
+test_that("odb_get_taxons works with defaults", { 
+    tx = odb_get_taxons(list(limit=10))
+    expect_is(tx, "data.frame")
+    expect_equal(nrow(tx), 10)
+})
+
+test_that("odb_get_taxons works with trailing slashes", { 
+    tx = odb_get_taxons(list(limit=10), odb_config("http://opendatabio.ib.usp.br/opendatabio/api/"))
+    expect_is(tx, "data.frame")
+    expect_equal(nrow(tx), 10)
+})
